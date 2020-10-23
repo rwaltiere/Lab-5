6819859a935281b817eb94facd8fcdf4d3fbed22
rwaltiere <71780179+rwaltiere@users.noreply.github.com>
2020-10-23 15:25:08 -0700

    Add files via upload

---
+++ lab5_supportfile.py
@@ -0,0 +1,36 @@
+banner_A = ["    ^    ", "   / \   ","  /   \  "," /=====\ ","||     ||","||     ||"]
+banner_B = ["@BBBBBB\ " , "|       }", "|       }", "@BBBBBBB@", "|       }","@BBBBBB/ "]
+banner_C = ["=========", "||       ", "||       ", "||       ", "||       ", "========="]
+banner_D = ["<<<<<<<\ ","^       )", "^       )", "^       )", "^       /", ">>>>>>>  "]
+banner_E = ["~~~~~~~~~","|        ", "|~~~~~~~~", "|        ", "|        ", "~~~~~~~~~"]
+banner_F = ["~~~~~~~~~","|        ", "|~~~~~~~~", "|        ", "|        ", "|        "]
+banner_G = ["*********", "*        ", "*        ", "*    ****", "*       *", "*********"]
+banner_H = ["@       @", "@       @", "@@@@@@@@@", "@       @", "@       @", "@       @"]
+banner_I = ["$$$$$$$$$", "    $    ", "    $    ", "    $    ", "    $    ", "$$$$$$$$$" ]
+banner_J = ["$$$$$$$$$", "    $    ", "    $    ", "    $    ", "    $    ", "$$$$$)   " ]
+banner_K = ["+       +", "+     +  ", "+ +      ", "+   +    ", "+      + ","+       +"]
+banner_L = ["^        ", "^        ", "^        ", "^        ", "^        ",  "@>>>>>>>>"] 
+banner_M = ["@MM   MM@", "M  \ /  M", "M   V   M", "M       M", "M       M", "@       @"]
+banner_N = 
+banner_O = 
+banner_P =
+banner_Q = 
+banner_R = 
+banner_S = 
+banner_T = 
+banner_U = 
+banner_V = 
+banner_W = 
+banner_X = 
+banner_Y =
+banner_Z =
+banner_space_char = ["         ", "         ", "         ", "         ", "        ", "        "]
+
+letter_to_banner = {'A': banner_A, 'B': banner_B, 'C': banner_C, 'D': banner_D,
+                    'E': banner_E, 'F': banner_F, 'G': banner_G, 'H': banner_H, 
+                    'I': banner_I, 'J': banner_J, 'K': banner_K, 'L': banner_L, 
+                    'M': banner_M, 'N': banner_N, 'O': banner_O, 'P': banner_P, 
+                    'Q': banner_Q, 'R': banner_R, 'S': banner_S, 'T': banner_T,
+                    'U': banner_U, 'V': banner_V, 'W': banner_W, 'X': banner_X,
+                    'Y': banner_Y, 'Z': banner_Z, ' ': banner_space_char }
+
